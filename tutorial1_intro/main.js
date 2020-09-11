356b22eed516b5c49858bceecea879b696b8cd01
David Leshinski <david@davids-air.myfiosgateway.com>
2020-09-11 10:35:34 -0400

    More style

--- tutorial1_intro/main.js
+++ tutorial1_intro/main.js
@@ -2,7 +2,11 @@ d3.csv("../data/surveyResults.csv").then(data => {
 
 const table = d3.select("#d3-table")
 .style("border-spacing", "0px")
-.style("padding", "0 15% 15% 15%");
+.style("width", "30%")
+.style("height", "800px")
+.style("padding", "8px")
+.style("background", "#c6c6c6")
+.style("margin", "0 auto");
 
 
 const thead = d3.select("#d3-table")
@@ -13,10 +17,10 @@ const headers = thead.selectAll(".header")
 .join("th")
 .attr("class","header")
 .text(d => d)
-.style("border-top", "1px solid #c6c6c6")
-.style("border-bottom", "1px solid #c6c6c6")
+.style("border-left", "1px solid #c6c6c6")
+.style("border-right", "1px solid #c6c6c6")
 .style("background", "#e0e0e0")
-.style("padding", "30px 30px");
+.style("padding", "0 30px");
 
 
 const tbody = table.append("tbody");
@@ -34,9 +38,9 @@ const td = rows
 .attr("class", d => +d > 3 ? 'high' : null)
 .text(d => d)
 .style("margin", "0px")
-.style("border-top", "1px solid #c6c6c6")
-.style("border-bottom", "1px solid #c6c6c6")
+.style("border-right", "1px solid #c6c6c6")
+.style("border-left", "1px solid #c6c6c6")
 .style("background", "#CFEEFA")
-.style("line-height", "1.5");
+// .style("line-height", "1.5");
 
 })
