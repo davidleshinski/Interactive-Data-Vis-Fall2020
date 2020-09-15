6b640f2c2da77008791997781659408709b16953
David Leshinski <david@Davids-MacBook-Air.local>
2020-09-15 15:26:09 -0400

    styled

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -1,16 +1,16 @@
 d3.csv("../data/City_populations.csv" , d3.autoType).then(data => {
 
-const svgWidth = 600
+const svgWidth = 725
 
-const svgHeight = 600
+const svgHeight = 475
 
-const margin = {top: 30, right: 20, bottom: 30, left: 100}
+const margin = {top: 30, right: 35, bottom: 30, left: 105}
 
 const innerWidth = svgWidth - margin.left - margin.right
 
 const innerHeight = svgHeight - margin.top - margin.bottom
 
-const accent = d3.scaleOrdinal(d3.schemeAccent);
+const blues = d3.scaleOrdinal(d3.schemeBlues[4]);
 
 const svg = d3.select(".d3-svg")
 .attr("width", svgWidth)
@@ -31,15 +31,18 @@ const g = svg.append("g")
 .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 
-g.append("g").call(d3.axisLeft(yScale));
+g.append("g").call(d3.axisLeft(yScale))
+.style('color','#fff');
+
 g.append("g").call(d3.axisBottom(xScale))
-.attr('transform', `translate(0, ${innerHeight})`);
+.attr('transform', `translate(0, ${innerHeight})`)
+.style('color','#fff');
 
 const bars = g.selectAll("rect")
 .data(data)
 .join("rect")
 .attr("class", "big-bars")
-.style("fill", (d,i) => accent(i))
+.style("fill", (d,i) => blues(i))
 .attr("y", d => yScale(d.name))
 // .attr("x", d => xScale(d.calories))
 .attr("width", d => xScale(d.population))
