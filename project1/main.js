5e8512d4d90da805a84028deed93eeafd7bd5cd4
David Leshinski <david@davids-air.myfiosgateway.com>
2020-11-02 18:15:48 -0500

    chart 1 done

--- project1/main.js
+++ project1/main.js
@@ -4,11 +4,10 @@
 const radius = 5,
  width = 800,
   height = 600,
-  margin = { top: 50, bottom: 30, left: 50, right: 90 },
+  margin = { top: 50, bottom: 50, left: 100, right: 90 },
   innerBoxHeight = height - margin.top - margin.bottom,
   innerBoxWidth = width - margin.left - margin.right,
   defaultGame = "All",
-  defaultViewer = "Twitch Viewership",
   defaultPlayerCount = "Peak24hrs"
 
 /** these variables allow us to access anything we manipulate in
@@ -20,13 +19,12 @@ let xAxis;
 let yAxis;
 let xScale;
 let yScale;
-let color;
+let myColor;
  
 let state = {
   accountData: [],
   gameData: [],
   gameSelection: defaultGame,
-  viewerSelection: defaultViewer
 };
 
 
@@ -52,7 +50,7 @@ Promise.all([
  * */
 function init() {
 
-  // ---------------------- game dropdown ----------------------
+  // ---------------------- game-dropdown-chart-1 ----------------------
 
     const selectGame = d3.select("#dropdown").on("change", function() {
         console.log(this.value)
@@ -69,7 +67,7 @@ function init() {
     .text(d => d);
 
 
-  // ---------------------- svg --------------------
+  // ---------------------- svg-chart-1 --------------------
 
     svg = d3.select('#d3-chart-1')
     .append('svg')
@@ -80,29 +78,47 @@ function init() {
     .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
 
-    // ----------------------- scales -------------------------
+    // ----------------------- scales-chart-1 -------------------------
 
     xScale = d3.scaleLinear()
-    .domain([d3.min(state.gameData.map(d =>d['Twitch Viewership'])), d3.max(state.gameData.map(d => d['Twitch Viewership']))])
+    .domain([d3.min(state.gameData.map(d =>d.Twitch24hrs)), d3.max(state.gameData.map(d => d.Twitch24hrs))])
     .range([0, innerBoxWidth])
 
     yScale = d3.scaleLinear()
-    .domain([d3.min(state.gameData.map(d =>d['Peak24hrs'])), d3.max(state.gameData.map(d => d['Peak24hrs']))])
+    .domain([d3.min(state.gameData.map(d =>d.Peak24hrs)), d3.max(state.gameData.map(d => d.Peak24hrs))])
     .range([innerBoxHeight, 0])
 
-// ----------------------- axis' -----------------------------
+     myColor = d3.scaleOrdinal()
+     .domain(state.gameData)
+    .range(d3.schemeSet3);
+
+// ----------------------- axis'-chart-1 -----------------------------
 
     yAxis = innerBox.append('g')
     .call(d3.axisLeft(yScale))
+    .attr('class', 'axis axis-left')
     .style('color', '#000')
+    .append('text')
+    .attr('class', 'axis-label-left')
+    .text('Peak conncurent Players in 24 hours')
+    .style('fill', '#000')
+    .attr("y", "50%")
+    .attr("dx", "-7em")
+    .attr("writing-mode", "vertical-lr")
+
 
     xAxis = innerBox.append('g')
     .call(d3.axisBottom(xScale))
-    .style('color', '#000')
     .attr('transform', `translate(0, ${innerBoxHeight})`)
+    .style('color', '#000')
+    .attr('class', 'axis axis-left')
     .append('text')
-    .text(state.viewerSelection)
- 
+    .attr('class', 'axis-label-bottom')
+    .text('Peak conncurent viewers in 24 hours')
+    .style('fill', '#000')
+    .attr("x", "40%")
+    .attr("dy", "4em")
+   
   // + ADD EVENT LISTENERS (if you want)
 
   draw(); // calls the draw function
@@ -114,6 +130,9 @@ function init() {
  * */
 
 function draw() {
+
+  // ----------------------- circles-charrt-1 --------------------------
+
   let filteredData = state.gameData;
   // if there is a selectedParty, filter the data before mapping it to our elements
   if (state.gameSelection !== defaultGame) {
@@ -121,9 +140,33 @@ function draw() {
   }
   dots = innerBox.selectAll('circle')
   .data(filteredData, d=> `${d.Name}_${d.Date}`)
-  .join('circle')
-  .attr('r', radius)
-  .attr('cx', d => xScale(d["Twitch Viewership"]))
-  .attr('cy', d => yScale(d.Peak24hrs))
-
+  .join( enter => enter
+    .append('circle')
+    .attr('cx', d => xScale(d.Twitch24hrs))
+    .attr('cy', d => yScale(d.Peak24hrs))
+    .attr("fill", d => myColor(d.Name))
+    .attr("stroke", d => myColor(d.Name))
+    .style('opacity', 0)
+    .attr('r', 0)
+    .call( enter => enter
+    .transition()
+    .duration(1000)
+      .style('opacity', 1)
+      .attr('r', radius)
+      ),
+      update => update 
+      .call( update => update
+        .transition()
+        .duration(500)
+      .attr('r', 3)
+      .transition()
+      .duration(500)
+      .attr('r', radius)),
+      exit => exit 
+      .call( exit => exit
+        .transition()
+        .duration(1000)
+        .style('opacity', 0)
+        .remove())
+      )
 }
