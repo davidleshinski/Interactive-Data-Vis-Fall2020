9b229bc54ec0e34e7f2897788d7df4529ab69386
David Leshinski <david@Davids-MacBook-Air.local>
2020-12-09 15:57:21 -0500

    tooltips

--- project2/main.js
+++ project2/main.js
@@ -4,13 +4,12 @@
 const radius = 6,
 width = 750,
 height = 500,
+treeHeight = 600,
+treeWidth = 800,
 margin = { top: 40, bottom: 60, left: 120, right: 90 },
 innerBoxHeight = height - margin.top - margin.bottom,
 innerBoxWidth = width - margin.left - margin.right;
 
-/** these variables allow us to access anything we manipulate in
- * init() but need access to in draw().
- * All these variables are empty before we assign something to them.*/
 let svgForLine;
 let boxForLine;
 let xScaleLine;
@@ -20,6 +19,7 @@ let yAxisLine;
 let marketLine;
 let marketDots;
 let lineFunc;
+let marketTooltip;
 
 let svgForBars;
 let yScaleForBars;
@@ -28,13 +28,15 @@ let boxForBars;
 let systemBars;
 let yAxisForBars;
 let xAxisForBars;
-let genderStats;
+let systemTooltip;
 
+let genderStats;
 let svgForLine2;
 let xScaleLine2;
 let xAxisForLine2;
 let yAxisForLine2;
 let genderDots;
+let genderTooltip;
 
 let svgForBars2;
 let boxForBars2;
@@ -43,11 +45,15 @@ let xScaleForBars2;
 let yAxisForBars2;
 let xAxisForBars2;
 let ageBars;
+let ageTooltip;
 
 let svgTree;
 let boxTree;
 let circlesTree;
-
+let simulation;
+let radiusScale;
+let treeText;
+let bubbleTooltip;
 
 let state = {
   ageData: [],
@@ -69,7 +75,6 @@ Promise.all([
         Value: +d.Value})),
     d3.csv("../data/gaming_rev_april2020.csv"),
     d3.csv("../data/worldwide_gamers.csv", d3.autoType)]).then(([ageData, genderData, marketData, aprilRevData, worldData]) => {
-    // + SET STATE WITH DATA
     state.ageData = ageData,
     state.genderData = genderData,
     state.marketData = marketData,
@@ -83,10 +88,10 @@ Promise.all([
     init();
   });
 
-// ---------------- init function start------------------
+
   function init() {
 
-// --------------------- svg-for-Market-line -------------------
+// --------------------- market -------------------
 
     svgForLine = d3.select('#d3-container-1')
     .append('svg')
@@ -96,8 +101,6 @@ Promise.all([
     boxForLine = svgForLine.append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`)
   
-// --------------------- scales-for-Market-line -------------------
-
     xScaleLine = d3.scaleTime()
     .domain(d3.extent(state.marketData.map(d => d.Year)))
     .range([0, innerBoxWidth])
@@ -106,37 +109,53 @@ Promise.all([
     .domain([0,210])
     .range([innerBoxHeight, 0])
 
-// --------------------- axis'-for-Market-line -------------------
 
     yAxisLine = boxForLine.append('g')
     .call(d3.axisLeft(yScaleLine))
-    .attr('class', 'axis axis-left')
+    .attr('class', 'axis')
     .style('color', '#rgb(62, 184, 62)')
-    .style('font-size','13px')
     .append('text')
 .attr("class", "axis-label")
 .attr("y", "55%")
 .attr("dx", "-4em")
 .attr("writing-mode", "vertical-lr")
 .style('fill','#000')
-.style('font-size','13px')
 .text("Dollars in Billions");
 
     xAxisLine = boxForLine.append('g')
     .call(d3.axisBottom(xScaleLine))
     .attr('transform', `translate(0, ${innerBoxHeight})`)
     .style('color', '#rgb(62, 184, 62)')
-    .attr('class', 'axis axis-left')
-    .style('font-size','13px')
+    .attr('class', 'axis')
     .append('text')
 .attr("class", "axis-label")
-.attr("x", "35%")
+.attr("x", "36%")
 .attr("dy", "3em")
 .style('fill','#000')
-.style('font-size','13px')
 .text("Year");
 
-// ----------------------- svg-for-system-bars ------------------------
+marketTooltip = d3.select('.market-tooltip')
+.style('visibility', 'hidden')
+
+marketMouseOver = (d) => {
+marketTooltip
+.style('visibility', 'visible')
+.style('opacity', '1')
+.style('background-color', '#fff')
+.style('border', '1px solid #000')
+.html('<span><strong>Year:</strong> ' + d.Year + '<br><strong>Value:</strong> ' + d.Value + ' billion</span>')
+.style("left", (d3.event.pageX + 10) + "px")
+.style("top", (d3.event.pageY -30) + "px")
+.style('padding', '5px 5px')
+.style('color', '#000')
+}
+
+marketMouseLeave = (d) => {
+  marketTooltip
+  .style('visibility', 'hidden')
+}
+
+// ----------------------- system ------------------------
 svgForBars = d3.select("#d3-container-2")
 .append("svg")
 .attr("width", width)
@@ -145,7 +164,6 @@ svgForBars = d3.select("#d3-container-2")
 boxForBars = svgForBars.append("g")
 .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-// ---------------------scale-for-system-bars----------------------
 
 xScaleForBars = d3.scaleBand()
 .domain(state.aprilRevData.map(d => d.system))
@@ -157,35 +175,53 @@ xScaleForBars = d3.scaleBand()
 .domain([0, d3.max(state.aprilRevData.map(d => d.total))])
 .range([innerBoxHeight, 0]);
 
-// ---------------------axis-for-system-bars----------------------
 
 yAxisForBars = boxForBars.append("g")
 .call(d3.axisLeft(yScaleForBars))
+.attr('class', 'axis')
 .style('color','#fff')
-.style('font-size','13px')
 .append('text')
 .attr("class", "axis-label")
 .attr("y", "55%")
 .attr("dx", "-4em")
 .attr("writing-mode", "vertical-lr")
 .style('fill','#fff')
-.style('font-size','13px')
 .text("Dollars in Billions");
 
 xAxisForBars = boxForBars.append("g")
 .call(d3.axisBottom(xScaleForBars))
+.attr('class', 'axis')
 .attr('transform', `translate(0, ${innerBoxHeight})`)
 .style('color','#fff')
-.style('font-size','13px')
 .append('text')
 .attr("class", "axis-label")
-.attr("x", "35%")
+.attr("x", "36%")
 .attr("dy", "3em")
 .style('fill','#fff')
-.style('font-size','13px')
 .text("Platform");
 
-// ---------------------svg-for-gender-line----------------------
+systemTooltip = d3.select('.system-tooltip')
+.style('visibility', 'hidden')
+
+systemMouseOver = (d) => {
+  systemTooltip
+  .style('visibility', 'visible')
+  .style('opacity', '1')
+  .style('background-color', '#fff')
+  .style('border', '1px solid #000')
+  .html('<span><strong>Platform:</strong> ' + d.system + ' <br><strong>Revenue:</strong> ' + d.total +  ' billion</span>')
+  .style("left", (d3.event.pageX + 10) + "px")
+.style("top", (d3.event.pageY -30) + "px")
+.style('padding', '5px 5px')
+.style('color', '#000')
+}
+
+systemMouseLeave = (d) => {
+  systemTooltip
+  .style('visibility', 'hidden')
+}
+
+// ---------------------gender----------------------
 
     genderStats = d3.nest() // nest function allows to group the calculation per level of a factor
     .key(function(d) { return d.gender;})
@@ -200,7 +236,6 @@ boxForLine2 = svgForLine2.append('g')
 .attr('transform', `translate(${margin.left}, ${margin.top})`)
 .attr('position', 'relative');
 
-// ---------------------scales-for-gender-line----------------------
 
    xScaleLine2 = d3.scaleTime()
     .domain(d3.extent(state.genderData, function(d) { return d.year; }))
@@ -212,22 +247,21 @@ boxForLine2 = svgForLine2.append('g')
     .domain([0, 100])
     .range([ innerBoxHeight, 0]);
 
-    // ---------------------axis'-for-gender-line----------------------
-
     xAxisForLine2 = boxForLine2.append("g")
     .call(d3.axisBottom(xScaleLine2))
+    .attr('class', 'axis')
     .attr('transform', `translate(0, ${innerBoxHeight})`)
     .style('color', '#000')
     .append('text')
 .attr("class", "axis-label")
-.attr("x", "35%")
+.attr("x", "36%")
 .attr("dy", "3em")
 .style('fill','#000')
-.style('font-size','13px')
 .text("Year");
 
   yAxisForLine2 = boxForLine2.append("g")
     .call(d3.axisLeft(yScaleLine2))
+    .attr('class', 'axis')
     .style('color', '#000')
     .append('text')
     .attr("class", "axis-label")
@@ -235,7 +269,6 @@ boxForLine2 = svgForLine2.append('g')
 .attr("dx", "-4em")
 .attr("writing-mode", "vertical-lr")
 .style('fill','#000')
-.style('font-size','13px')
 .text("Percentage of Players");
 
   // color palette
@@ -244,17 +277,28 @@ boxForLine2 = svgForLine2.append('g')
     .domain(genderGroups)
     .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
 
-  // legendForLines2 = boxForLine2.append('div')
-  // .attr('position', 'absolute')
-  // .attr('bottom', '0')
-  // .attr('right', '0')
-  // .attr('class', 'legend-for-gender')
-  // .append('p')
-  // .text('Male')
-  // .style('font-size', '50px')
-  // .style('color', '#000')
+  genderTooltip = d3.select('.gender-tooltip')
+  .style('visibility', 'hidden')
+
+  genderMouseOver = (d) => {
+    genderTooltip
+    .style('visibility', 'visible')
+    .style('opacity', '1')
+    .style('background-color', '#fff')
+    .style('border', '1px solid #000')
+    .html('<span> <strong>Year:</strong> ' + d.year + '<br><strong>Gender:</strong> ' + d.gender + ' <br><strong>Percentage of Gamers:</strong> ' + d.percentage +  '%</span>')
+    .style("left", (d3.event.pageX + 10) + "px")
+  .style("top", (d3.event.pageY -30) + "px")
+  .style('padding', '5px 5px')
+  .style('color', '#000')
+  }
   
-// ----------------------- svg-for-age-bars------------------------
+genderMouseLeave = (d) => {
+  genderTooltip
+  .style('visibility', 'hidden')
+}
+
+// ----------------------- age ------------------------
 svgForBars2 = d3.select("#d3-container-4")
 .append("svg")
 .attr("width", width)
@@ -263,7 +307,6 @@ svgForBars2 = d3.select("#d3-container-4")
 boxForBars2 = svgForBars2.append("g")
 .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-// ---------------------scale-for-age-bars----------------------
 
 xScaleForBars2 = d3.scaleBand()
 .domain(state.ageData.map(d => d.range))
@@ -276,59 +319,119 @@ xScaleForBars2 = d3.scaleBand()
 .domain([0, d3.max(state.ageData.map(d => d.percent))])
 .range([innerBoxHeight, 0]);
 
-// ---------------------axis-for-age-bars----------------------
 
 yAxisForBars2 = boxForBars2.append("g")
 .call(d3.axisLeft(yScaleForBars2))
-.style('font-size','13px')
+.attr('class', 'axis')
 .append('text')
 .attr("class", "axis-label")
 .attr("y", "55%")
 .attr("dx", "-4em")
 .attr("writing-mode", "vertical-lr")
 .style('fill','#fff')
-.style('font-size','13px')
 .text("Percentage of Players");
 
 xAxisForBars2 = boxForBars2.append("g")
 .call(d3.axisBottom(xScaleForBars2))
+.attr('class', 'axis')
 .attr('transform', `translate(0, ${innerBoxHeight})`)
-.style('font-size','13px')
 .append('text')
 .attr("class", "axis-label")
 .attr("x", "32%")
 .attr("dy", "4em")
 .text("Age Range")
-.style('font-size','13px')
 .style('fill', '#fff')
 ;
 
-// -----------------bubbles-------------------------
+ageTooltip = d3.select('.age-tooltip')
+.style('visibility', 'hidden')
+
+ageMouseOver = (d) => {
+  ageTooltip
+  .style('visibility', 'visible')
+  .style('opacity', '1')
+  .style('background-color', '#fff')
+  .style('border', '1px solid #000')
+  .html('<span><strong>Age Range:</strong> ' + d.range + ' <br><strong>Percentage of Gamers:</strong> ' + d.percent +  '%</span>')
+  .style("left", (d3.event.pageX + 10) + "px")
+.style("top", (d3.event.pageY -30) + "px")
+.style('padding', '5px 5px')
+.style('color', '#000')
+}
+
+ageMouseLeave = (d) => {
+  ageTooltip
+  .style('visibility', 'hidden')
+  }
+
+// ----------------- worldwide-------------------------
 
 svgTree = d3.select('#d3-container-5')
 .append("svg")
-.attr('height', height)
-.attr('width', width)
+.attr('height', treeHeight)
+.attr('width', treeWidth)
 
 boxTree = svgTree.append("g")
 .attr("transform", "translate(0,0)")
 
+radiusScale = d3.scaleSqrt()
+.domain([210, 1447])
+.range([50,125])
 
-draw();
+
+simulation = d3.forceSimulation()
+.force("x", d3.forceX(treeWidth *2)
+.strength(0.005))
+.force("y", d3.forceY(treeHeight * 2)
+.strength(0.005))
+.force("collide", d3.forceCollide(d => radiusScale(d.players)))
+
+
+simulation.nodes(state.worldData)
+.on('tick', ticked)
+
+bubbleTooltip = d3.select('.bubble-tooltip')
+.style('visibility', 'hidden')
+
+bubbleMouseOver = (d) => {
+  bubbleTooltip
+  .style('visibility', 'visible')
+  .style('opacity', '1')
+  .style('background-color', '#fff')
+  .style('border', '1px solid #000')
+  .html('<span><strong>Region:</strong> ' + d.region + ' <br><strong>Gamers:</strong> ' + d.players +  ' million</span>')
+  .style("left", (d3.event.pageX + 10) + "px")
+.style("top", (d3.event.pageY -30) + "px")
+.style('padding', '5px 5px')
+.style('color', '#000')
 }
 
-  // --------------------- function-for-Market-line-------------------
+bubbleMouseLeave = (d) => {
+bubbleTooltip
+.style('visibility', 'hidden')
+}
+
+draw();
+  } 
+
+  // --------------------- functions that im not sure where to put (in init or draw?)-------------------
+  
+function ticked() {
+  circlesTree
+  .attr("cx", d => d.x)
+  .attr("cy", d => d.y)
+}
+  
 
   lineFunc = d3.line()
   .x(d => xScaleLine(d.Year))
   .y(d => yScaleLine(d.Value));
 
+  //-------------
   
-  
-// ---------------- draw function start------------------
   function draw() {
 
-    // --------------------- line+dots-for-Market-Line -------------------
+    // ---------------------Market-Line -------------------
 
 marketLine = boxForLine.selectAll('.trend')
 .data([state.marketData])
@@ -346,8 +449,10 @@ marketDots = boxForLine.selectAll('circle')
 .attr('cx', d => xScaleLine(d.Year))
 .attr('cy', d => yScaleLine(d.Value))
 .attr('r', 4)
+.on('mouseover', marketMouseOver)
+.on('mouseleave', marketMouseLeave)
 
-// ------------------------- bars-for-System-bars----------------------------
+// -------------------------System-bars----------------------------
  systemBars = boxForBars.selectAll("rect")
 .data(state.aprilRevData)
 .join("rect")
@@ -357,9 +462,11 @@ marketDots = boxForLine.selectAll('circle')
 .style('fill', '#fff')
 .attr("x", d => xScaleForBars(d.system))
 .attr("height", d => innerBoxHeight - yScaleForBars(d.total))
-.attr("width", d => xScaleForBars.bandwidth());
+.attr("width", d => xScaleForBars.bandwidth())
+.on('mouseover', systemMouseOver)
+.on('mouseleave', systemMouseLeave)
 
-//------------------------- lines+dots-for-gender-lines------------------------
+//-------------------------gender-lines------------------------
 
 boxForLine2.selectAll(".line")
 .data(genderStats)
@@ -383,9 +490,11 @@ boxForLine2.selectAll(".line")
 .attr('cx', d => xScaleLine2(d.year))
 .attr('cy', d => yScaleLine2(d.percentage))
 .attr('r', 4)
+.on('mouseover', genderMouseOver)
+.on('mouseleave', genderMouseLeave)
 
 
-// ------------------------- bars-for-age-bars----------------------------\
+// -------------------------age-bars----------------------------\
 
 ageBars= boxForBars2.selectAll(".big-bars2")
 .data(state.ageData)
@@ -396,12 +505,21 @@ ageBars= boxForBars2.selectAll(".big-bars2")
 .attr("y", d => yScaleForBars2(d.percent))
 .attr("x", d => xScaleForBars2(d.range))
 .attr("height", d => innerBoxHeight - yScaleForBars2(d.percent))
-.attr("width", d => xScaleForBars2.bandwidth());
+.attr("width", d => xScaleForBars2.bandwidth())
+.on('mouseover', ageMouseOver)
+.on('mouseleave', ageMouseLeave);
+
+
+// -----------------------bubble-circles----------------------
  
 circlesTree = boxTree.selectAll(".tree-circles")
 .data(state.worldData)
 .join('circle')
 .attr("class", "tree-circles")
-.attr("r", 10)
-.attr("fill", 'lightblue')
+.attr("r", d => radiusScale(d.players))
+.attr("fill", 'rgb(62, 184, 62)')
+.on("mouseover", bubbleMouseOver)
+.on("mouseleave", bubbleMouseLeave)
 }
+
+
